d676815ffc08fbcf74521f0a12239293f641b3a8
Nur Afandi Mujib <fndmujib@gmail.com>
2020-10-23 19:57:32 +0700

    Push Tugas

--- Employee/Controllers/EmployeesController.cs
+++ Employee/Controllers/EmployeesController.cs
@@ -43,9 +43,9 @@ namespace Employee.Controllers
         }
 
         // GET: Employees/Create
-        public IActionResult Create()
+        public IActionResult Create(int id=0)
         {
-            return View();
+            return View(new Employees());
         }
 
         // POST: Employees/Create
@@ -118,19 +118,10 @@ namespace Employee.Controllers
         // GET: Employees/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees
-                .FirstOrDefaultAsync(m => m.EmployeeId == id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-
-            return View(employees);
+            var employees = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employees);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: Employees/Delete/5
